d4f75c2a96e43dd19e918a00c600aadfb837ba9c
Guanyi <guanyifang@hotmail.com>
2013-11-24 02:23:29 -0800

    Added new java file Poem.java

---
+++ Poem.java
@@ -0,0 +1,16 @@
+
+// ************************************************************
+// Poem.java
+// Name: Guanyi Fang
+// Print a poem in a certain format 
+// ************************************************************
+
+public class Poem {
+    
+    public static void main (String[] args) {
+        System.out.println("Rose are red\n" + 
+                           "Violets are blue\n" +
+                           "Suger is sweet\n" +
+                           "And so are you!");
+    }
+}
